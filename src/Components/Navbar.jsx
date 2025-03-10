9838984b4e4da8b5964f169a1f0b24016420c7cd
SandaminaWijenayake <sandaminawijenayake0717@gmail.com>
2025-03-10 20:35:58 +0530

    kjna
    sgsdf

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router";
 import MenuIcon from "@mui/icons-material/Menu";
+import logo from "../assets/images/Logo2Final.png";
 
 export default function Navbar() {
   return (
     <nav className="w-full px-[20px] pt-[20px] pb-[15px] fixed top-0 z-10 bg-black/95">
       <div className=" max-w-[1290px] h-[46px] m-auto font-robotoSlab font-normal text-[15px]  flex justify-between items-center">
-        <div className="text-lg font-bold text-white">Logo</div>
+        <div className="w-[60px] font-bold text-white">
+          <img src={logo} alt="" />
+        </div>
         <div className="md:flex space-x-6  group  hidden [&>*:hover]:text-[#ffd900]">
           <NavLink
             className={({ isActive }) =>
